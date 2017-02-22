7156ca862f70b1da5c3634dabdbe6313520b4682
Benjamin <benjamin.nutter@gmail.com>
2017-02-22 09:33:00 -0500

    Create toggle_plural.R

---
+++ R/toggle_plural.R
@@ -0,0 +1,52 @@
+#' @name toggle_plural
+#' @title Toggle Between Plural and Singular Word Choice
+#'
+#' @description When writing dynamic documents, it is not always known if a 
+#'  value will be singular or plural.  Writing a document that accommodates
+#'  both conditions can be challenging.  \code{toggle_plural} assists
+#'  by giving the user a choice of words, and inserts the appropriate
+#'  word based on the value being printed.
+#' 
+#' @param x \code{numeric(1)}
+#' @param singular \code{character(1)}, defines the word choice to use if 
+#'  \code{x} is equal to 1.
+#' @param plural \code{character(1)}, defines the word choice to use if
+#'   \code{x} is not equal to 1.
+#' @param ... Additional arguments to pass to \code{\link{format}}
+#' 
+#' @author Benjamin Nutter
+#' 
+#' @return Returns a \code{character(1)} object.
+#'
+#' @seealso \code{\link{toggle_text}}, \code{\link{format}}, \code{\link{all.equal}}, 
+#'   \code{\link{isTRUE}}
+#' 
+#' @examples
+#' toggle_plural(1, "was", "were")
+#'
+#' toggle_plural(1, "is", "are")
+#' 
+#' @export
+
+toggle_plural <- function(x, singular, plural, ...)
+{
+  coll <- checkmate::makeAssertCollection()
+  
+  checkmate::assert_numeric(x = x,
+                            len = 1,
+                            add = coll)
+  
+  checkmate::assert_character(x = singular,
+                              len = 1,
+                              add = coll)
+  
+  checkmate::assert_character(x = plural,
+                              len = 1,
+                              add = coll)
+  
+  checkmate::reportAssertions(coll)
+  
+  sprintf("%s %s",
+          if(isTRUE(all.equal(x, 1))) singular else plural,
+          format(x, ...))
+}
